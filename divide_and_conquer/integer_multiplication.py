2733667d6f3a207f57c9c7dd492855b05d6f3224
Prashanth Palaniappan <prashanthrulz@gmail.com>
2019-03-04 20:24:14 -0800

    Integer multiplication - adding support for odd input sizes

--- divide_and_conquer/integer_multiplication.py
+++ divide_and_conquer/integer_multiplication.py
@@ -1,18 +1,18 @@
 # Integer multiplication using recursion
 # Author: Prashanth Palaniappan
 
-'''
+"""
 Description:
 This is an algorithm that performs multiplication of 2 integers of size n using recursion.
 
 Solution:
 The algorithm recursively divides the input numbers by half, until we are left with single digits. This is the base
 case for recursion. Once we hit the base case, we perform regular multiplication of the single digits
-'''
+"""
 
 
 def multiply(x, y, n):
-    '''
+    """
     :param x: input number 1
     :param y: input number 2
     :param n: length of the input numbers
@@ -25,7 +25,7 @@ def multiply(x, y, n):
 
     x * y   = ((10^n/2 * a) + b) * ((10^n/2 * c) + d)
             = (10^n * ac) + (10^n/2 * (ad + bc)) + bd
-    '''
+    """
 
     if n == 1:
         return x * y
@@ -33,16 +33,22 @@ def multiply(x, y, n):
     b = x - (a * pow(10, n//2))
     c = y // pow(10, n//2)
     d = y - (c * pow(10, n//2))
-    ac = multiply(a, c, n//2)
-    bd = multiply(b, d, n//2)
-    bc_plus_ad = multiply(a + b, c + d, n//2) - ac - bd
-    mul_result = (pow(10, n) * ac) + (pow(10, n//2) * bc_plus_ad) + bd
+    ac = multiply(a, c, len(str(a)))
+    bd = multiply(b, d, len(str(b)))
+    bc_plus_ad = multiply(a + b, c + d, len(str(a + b))) - ac - bd
+    mul_result = (pow(10, n//2) * pow(10, n//2) * ac) + (pow(10, n//2) * bc_plus_ad) + bd
     return mul_result
 
 
 # Get the input numbers from an user
 x = int(input('First number: '))
 y = int(input('Second number: '))
+
+# Raise exception if input integers are not of equal length
+if len(str(x)) != len(str(y)):
+    raise Exception('Please enter 2 integers of equal length')
+
+# Get the size of input
 n = len(str(x))
 
 # Compute and print the result
